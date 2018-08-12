fcc321dbed02a94c95cf0c32eb81a8ac394da132
user <user@LAPTOP-4SJO4KOR>
2018-08-12 13:28:48 +0100

    Finds item title and effects and displays in console and title on screen displays an image from URL within jlabel on screen item effects found by scraping webpage, finding the bodyContent id then selecting the effects UL and adds all of the list within it to an array

--- src/BindingOfIsaacForm/BindingOfIsaacItemForm.java
+++ src/BindingOfIsaacForm/BindingOfIsaacItemForm.java
@@ -7,6 +7,17 @@
 package BindingOfIsaacForm;
 
 import BindingOfIsaacPackage.item;
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 
 
 /**
@@ -34,7 +45,17 @@ public class BindingOfIsaacItemForm extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         btnFinish = new javax.swing.JButton();
         txtItemName = new javax.swing.JTextField();
-        jLabel2 = new javax.swing.JLabel();
+        containerItemInformation = new javax.swing.JPanel();
+        txtItemTitle = new javax.swing.JLabel();
+        imgItemIcon = new javax.swing.JLabel();
+        txtItemTitle2 = new javax.swing.JLabel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTextArea2 = new javax.swing.JTextArea();
+        txtItemEffects = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jList1 = new javax.swing.JList<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -47,7 +68,78 @@ public class BindingOfIsaacItemForm extends javax.swing.JFrame {
             }
         });
 
-        jLabel2.setText("jLabel2");
+        txtItemName.setText("Brother Bobby");
+        txtItemName.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtItemNameActionPerformed(evt);
+            }
+        });
+
+        txtItemTitle.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
+        txtItemTitle.setText("jLabel2");
+
+        imgItemIcon.setText("jLabel2");
+        imgItemIcon.setMaximumSize(new java.awt.Dimension(50, 50));
+        imgItemIcon.setMinimumSize(new java.awt.Dimension(50, 50));
+
+        txtItemTitle2.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        txtItemTitle2.setText("Notes:");
+
+        jTextArea2.setColumns(20);
+        jTextArea2.setRows(5);
+        jScrollPane2.setViewportView(jTextArea2);
+
+        txtItemEffects.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        txtItemEffects.setText("Effects:");
+
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
+
+        javax.swing.GroupLayout containerItemInformationLayout = new javax.swing.GroupLayout(containerItemInformation);
+        containerItemInformation.setLayout(containerItemInformationLayout);
+        containerItemInformationLayout.setHorizontalGroup(
+            containerItemInformationLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(containerItemInformationLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(containerItemInformationLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1)
+                    .addComponent(jScrollPane2)
+                    .addGroup(containerItemInformationLayout.createSequentialGroup()
+                        .addGroup(containerItemInformationLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(txtItemTitle2)
+                            .addComponent(txtItemEffects)
+                            .addGroup(containerItemInformationLayout.createSequentialGroup()
+                                .addComponent(txtItemTitle)
+                                .addGap(52, 52, 52)
+                                .addComponent(imgItemIcon, javax.swing.GroupLayout.PREFERRED_SIZE, 59, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(0, 360, Short.MAX_VALUE)))
+                .addContainerGap())
+        );
+        containerItemInformationLayout.setVerticalGroup(
+            containerItemInformationLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(containerItemInformationLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(containerItemInformationLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtItemTitle)
+                    .addComponent(imgItemIcon, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 19, Short.MAX_VALUE)
+                .addComponent(txtItemEffects)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 117, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(txtItemTitle2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 117, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+
+        jList1.setModel(new javax.swing.AbstractListModel<String>() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public String getElementAt(int i) { return strings[i]; }
+        });
+        jScrollPane3.setViewportView(jList1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -56,28 +148,33 @@ public class BindingOfIsaacItemForm extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 261, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE))
                     .addComponent(btnFinish, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel1)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(txtItemName, javax.swing.GroupLayout.DEFAULT_SIZE, 525, Short.MAX_VALUE))
+                        .addComponent(txtItemName, javax.swing.GroupLayout.DEFAULT_SIZE, 546, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel2)
-                        .addGap(0, 0, Short.MAX_VALUE)))
+                        .addGap(60, 60, 60)
+                        .addComponent(containerItemInformation, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(54, 54, 54)
+                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
                     .addComponent(txtItemName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(btnFinish)
-                .addGap(18, 18, 18)
-                .addComponent(jLabel2)
-                .addContainerGap(275, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 124, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 28, Short.MAX_VALUE)
+                .addComponent(containerItemInformation, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(10, 10, 10))
         );
 
         pack();
@@ -92,11 +189,37 @@ public class BindingOfIsaacItemForm extends javax.swing.JFrame {
         
         item i = new item(itemName);
         
+        txtItemEffects.setText(i.getItemTitle());
         
+        Image image = null;
+        Dimension imgSize = new Dimension (51, 20);
         
+        URL url;
+        try {
+            url = new URL("https://d1u5p3l4wpay3k.cloudfront.net/bindingofisaacre_gamepedia/3/33/Brother_Bobby_Icon.png?version=c3717cf922def24e7eecac1a6df8cbf8");
+            image = ImageIO.read(url);
+            
+            imageUpdate(image, NORMAL, WIDTH, WIDTH, 500, 500);
+            imgItemIcon.setIcon(new ImageIcon(image));
+            
+
+
+            
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(BindingOfIsaacItemForm.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(BindingOfIsaacItemForm.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        
+
         
         
     }//GEN-LAST:event_btnFinishActionPerformed
+
+    private void txtItemNameActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtItemNameActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtItemNameActionPerformed
 
     /**
      * @param args the command line arguments
@@ -135,8 +258,18 @@ public class BindingOfIsaacItemForm extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnFinish;
+    private javax.swing.JPanel containerItemInformation;
+    private javax.swing.JLabel imgItemIcon;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
+    private javax.swing.JList<String> jList1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JTextArea jTextArea2;
+    private javax.swing.JLabel txtItemEffects;
     private javax.swing.JTextField txtItemName;
+    private javax.swing.JLabel txtItemTitle;
+    private javax.swing.JLabel txtItemTitle2;
     // End of variables declaration//GEN-END:variables
 }
